0bc368520e9896d4499997175e58ce409a6d7929
LoneExile <apinant-moo@tutanota.com>
2022-10-11 15:05:10 +0700

    zustand examble add type

--- components/zustand.ts
+++ components/zustand.ts
@@ -1,9 +1,14 @@
 import create from 'zustand'
 
-const counterStore = (set) => ({
+const counterStore = (
+  set: (arg0: {
+    (state: any): {count: number}
+    (state: any): {count: number}
+  }) => any
+) => ({
   count: 0,
-  increment: () => set((state) => ({count: state.count + 1})),
-  decrement: () => set((state) => ({count: state.count - 1}))
+  increment: () => set((state: any) => ({count: state.count + 1})),
+  decrement: () => set((state: any) => ({count: state.count - 1}))
 })
 
 export const useCounterStore = create(counterStore)
